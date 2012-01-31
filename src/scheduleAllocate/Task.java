e192bf843541d3b4468cb6bab6ef2f4651f1682f
zhangchang <zhangchang@163.com>
2012-01-31 10:27:42 +0900

    変数追加

--- src/scheduleAllocate/Task.java
+++ src/scheduleAllocate/Task.java
@@ -3,6 +3,8 @@
  */
 package scheduleAllocate;
 
+import java.util.Date;
+
 /**
  * @author zhangchang
  *
@@ -14,5 +16,29 @@ public class Task {
 	 */
 	private String taskName = null;
 	
+	/**
+	 * 
+	 */
 	private Integer taskNo = null;
+	
+	/**
+	 * 
+	 */
+	private Date taskStart = null;
+	
+	/**
+	 * 
+	 */
+	private Date taskEnd = null;
+	
+	/**
+	 * 
+	 */
+	private double taskCost = 0.0;
+	
+	/**
+	 * 
+	 */
+	private Task prevTask = null;
+	
 }
