a459cc5972426729d40f4e21bc31517e246c2cf9
zhangchang <zhangchang@163.com>
2012-01-31 10:18:07 +0900

    aaa

---
+++ src/scheduleAllocate/Task.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package scheduleAllocate;
+
+/**
+ * @author zhangchang
+ *
+ */
+public class Task {
+
+	/**
+	 * 
+	 */
+	private String taskName = null;
+	
+	private Integer taskNo = null;
+}
