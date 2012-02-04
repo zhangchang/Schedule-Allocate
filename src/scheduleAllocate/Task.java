54a63a87ea016d4f925cc7cfdc2a152a7ffe1bcf
Chang Zhang <zhangchang1979@gmail.com>
2012-02-04 22:52:50 +0800

    追加注释

--- src/scheduleAllocate/Task.java
+++ src/scheduleAllocate/Task.java
@@ -12,32 +12,32 @@ import java.util.Date;
 public class Task {
 
 	/**
-	 * 
+	 * 任务名称
 	 */
 	private String taskName = null;
 	
 	/**
-	 * 
+	 * 任务ID
 	 */
 	private Integer taskNo = null;
 	
 	/**
-	 * 
+	 * 任务开始日
 	 */
 	private Date taskStart = null;
 	
 	/**
-	 * 
+	 * 任务完成日
 	 */
 	private Date taskEnd = null;
 	
 	/**
-	 * 
+	 * 任务工时（单位：人日）
 	 */
 	private double taskCost = 0.0;
 	
 	/**
-	 * 
+	 * 前导任务
 	 */
 	private Task prevTask = null;
 	
