addc786d1613938c7f8f64611f9560013d6babe8
Zhang Chang <zhangchang@gmail.com>
2012-02-08 23:06:39 +0800

    常量追加

--- src/scheduleAllocate/CommonConst.java
+++ src/scheduleAllocate/CommonConst.java
@@ -22,10 +22,30 @@ public class CommonConst {
 	/**
 	 * 成员职责：测试CASE作成
 	 */
-	public static String MEMBER_DUTY_TESTCASEMAKE = "TestCaseMake";
+	public static String MEMBER_DUTY_TESTCASE = "TestCase";
 	
 	/**
 	 * 成员职责：测试CASE实施
 	 */
 	public static String MEMBER_DUTY_TESTEXEC = "TestExec";
+	
+	/**
+	 * 成员职责：设计审核
+	 */
+	public static String MEMBER_DUTY_DESIGN_REVIEW = "DesignReview";
+	
+	/**
+	 * 成员职责：开发审核
+	 */
+	public static String MEMBER_DUTY_DEVELOP_REVIEW = "DevelopReview";
+	
+	/**
+	 * 成员职责：测试CASE审核
+	 */
+	public static String MEMBER_DUTY_TESTCASE_REVIEW = "TestCaseReview";
+	
+	/**
+	 * 成员职责：测试实施审核
+	 */
+	public static String MEMBER_DUTY_TESTEXEC_REVIEW = "TestExecReview";
 }
