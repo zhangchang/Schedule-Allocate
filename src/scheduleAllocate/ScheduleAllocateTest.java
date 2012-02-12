c4c77253a8a29e8816997d0fd6456d3e985706fd
Chang Zhang <zhangchang1979@gmail.com>
2012-02-12 10:33:43 +0800

    source add

---
+++ src/scheduleAllocate/ScheduleAllocateTest.java
@@ -0,0 +1,13 @@
+package scheduleAllocate;
+
+public class ScheduleAllocateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/scheduleAllocate/WorkCategory.java
@@ -0,0 +1,5 @@
+package scheduleAllocate;
+
+public class WorkCategory {
+
+}
